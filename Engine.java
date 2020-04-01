82e81ca9ade6452054e4f0117d690d39ff34e9c3
wnsghk16 <wnsghk16@naver.com>
2020-04-01 18:27:22 +0900

    200401 18:27

--- Engine.java
+++ Engine.java
@@ -6,16 +6,23 @@ public class Engine {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		while(true) {
-			System.out.println("0.종료 1.가위바위보 게임 2.주사위 홀짝 게임");
+			System.out.println("\n아래의 숫자중에서 선택해주세요.");
+			System.out.println("0.종료 1.가위바위보 2.주사위 홀짝맞추기 3.1부터n까지합 4.성별체크 5.별만들기");
 			int flag = scanner.nextInt();
 			switch(flag) {
-			case 0: System.out.println("종료"); 
-					return;
-			case 1: System.out.println("계산기");
-					break;
+			case 0: System.out.println("종료합니다."); 
+				return;
+			case 1: game();
+				break;
 			case 2: dice();
-					break;
-			
+				break;
+			case 3: sum();
+				break;
+			case 4: find();
+				break;
+			case 5:
+				star();
+				break;			
 			}
 		}
 	}
@@ -31,27 +38,119 @@ public class Engine {
 		System.out.println(String.format("실제 주사위값 : %d",dice));
 		
 		String result="";
-		switch(dice) {
-		case 2: case 4: case 6:
-			result="짝";
-			break;
-		case 1: case 3: case 5:
-			result="홀";
-			break;
-		}
-		System.out.println(String.format("결과값은 %s수입니다.",result));
-		/*
+	
 		if(dice%2==0){
 			result="짝";
 		}else{
 			result="홀";
 		}
 		System.out.println(String.format("결과값은 %s수입니다.",result));
-		 */		
+			
 		String fin = "오답";
 		if(choice.equals(result)) {	
 			fin = "정답";
 		}
 		System.out.println(String.format("%s입니다.",fin));			
+	}
+	static void game() {
+		System.out.println("가위1, 바위2, 보3을 입력하세요");
+		Scanner scanner = new Scanner(System.in);
+		int user = scanner.nextInt();
+		
+		System.out.println(String.format("사용자의 값은 %d입니다.", user));
+		
+		Random random = new Random();
+		int com = random.nextInt(3)+1;
+		System.out.println(String.format("컴퓨터의 값은 %d입니다.", com));
+
+		int flag = 0;
+		
+		if(user == com) {
+			flag = 0;
+		}else if((user==1&&com==2) | (user==2&&com==3) | (user==3&&com==1)) {
+			flag = 1;
+		}else if((user==1&&com==3) | (user==2&&com==1) | (user==3&&com==2)) {
+			flag = 2;
+		}else {
+			System.out.println("잘못입력했습니다.");
+			return;
+		}
+	
+		switch(flag) {
+		case 0:
+			System.out.println("비겼습니다.");
+			break;
+		case 1: 
+			System.out.println("당신이 졌습니다.");
+			break;
+		case 2: 
+			System.out.println("당신이 이겼습니다.");
+			break;
+		}		
+	}
+	static void sum() {
+		System.out.println("1부터 n까지 합 구하기");
+		System.out.println("n을 입력해 주세요");
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int sum=0;
+		
+		for(int i=1;i<=n;i++) {
+			sum += i;
+		}
+		
+		System.out.println(String.format("1부터 %d까지 합 = %d", n, sum));
+		
+	}
+	static void find() {
+		System.out.println("주민번호를 입력하세요 ex)900101-123456");
+		Scanner scanner = new Scanner(System.in);
+		String ssn = scanner.next();//"900101-123456"
+		System.out.println(String.format("당신의 주민번호는 %s입니다.",ssn));
+		char ch = ssn.charAt(7);
+		
+		String gender="";
+		switch(ch) {
+		case '1': case'3':
+			gender = "남성";//1,3
+			break;
+		case '2': case'4':
+			gender = "여성";//2,4
+			break;
+		case '5': case'6':
+			gender = "외국인";//5,6
+			break;
+		default:
+			gender = "잘못입력한 값";//0,7,8,9
+			break;			
+		}
+		
+		System.out.println(String.format("당신은 %s입니다.",gender));
+					
+	}
+	static void star() {
+		System.out.print("n입력 : ");
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		for(int i=0; i<n/2+1; i++) {
+			for(int j=n/2+1; j>i; j--) {
+				System.out.print(" ");	
+			}
+			for(int k=1; k<=(2*i+1); k++) {
+				System.out.print("*");	
+			}			
+			System.out.print("\n");
+			
+		}
+		for(int i=n; i>n/2+1; i--) {
+			for(int j=i-1; j<=n; j++) {
+				System.out.print(" ");	
+			}
+			for(int k=n; k<=(2*i-3); k++) {
+				System.out.print("*");	
+			}			
+			System.out.print("\n");
+		}		
 	}
 }
--- RPSgame.java
+++ RPSgame.java
@@ -10,6 +10,7 @@ public class RPSgame {
 		System.out.println("가위1, 바위2, 보3을 입력하세요");
 		Scanner scanner = new Scanner(System.in);
 		int user = scanner.nextInt();
+		
 		System.out.println(String.format("사용자의 값은 %d입니다.", user));
 		
 		Random random = new Random();
@@ -17,69 +18,28 @@ public class RPSgame {
 		System.out.println(String.format("컴퓨터의 값은 %d입니다.", com));
 
 		int flag = 0;
-		swcom(user,com,flag);
-				
+		
+		if(user == com) {
+			flag = 0;
+		}else if((user==1&&com==2) | (user==2&&com==3) | (user==3&&com==1)) {
+			flag = 1;
+		}else if((user==1&&com==3) | (user==2&&com==1) | (user==3&&com==2)) {
+			flag = 2;
+		}else {
+			System.out.println("잘못입력했습니다.");
+			return;
+		}
+	
 		switch(flag) {
 		case 0:
 			System.out.println("비겼습니다.");
 			break;
 		case 1: 
-			System.out.println("당신이 이겼습니다.");
-			break;
-		case 2: 
 			System.out.println("당신이 졌습니다.");
 			break;
+		case 2: 
+			System.out.println("당신이 이겼습니다.");
+			break;
 		}		
-	}
-	static int swcom(int user, int com, int flag) {
-		if(user==1) {
-			switch(com) {
-			case 1:
-				flag = 0; // 비김
-				break;
-			case 2:
-				flag = 1; //짐
-				break;
-			case 3:
-				flag = 2; //이김
-				break;
-			default:
-				System.out.println("잘못입력했습니다.");
-				break;
-			}
-		}else if(user==2) {
-			switch(com) {
-			case 1:
-				flag = 2;
-				break;
-			case 2:
-				flag = 0;
-				break;
-			case 3:
-				flag = 1;
-				break;
-			default:
-				System.out.println("잘못입력했습니다.");
-				break;
-			}
-		}else if(user==3) {
-			switch(com) {
-			case 1:
-				flag = 1;
-				break;
-			case 2:
-				flag = 2;
-				break;
-			case 3:
-				flag = 0;
-				break;
-			default:
-				System.out.println("잘못입력했습니다.");
-				break;
-			}
-		}else {
-			System.out.println("잘못입력했습니다.");
-		}
-		return flag;
 	}
 }
